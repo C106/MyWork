763b7b3dbbbc53ce617674fe40a6c43fef0e65fc
C106 <38374631+C106@users.noreply.github.com>
2024-05-19 02:32:58 +0800

    da

--- Project1/FileName.cpp
+++ Project1/FileName.cpp
@@ -1,6 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -20,12 +20,14 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+
+    glewInit();
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
